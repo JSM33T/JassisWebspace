6d07f0f68f900e52a403c2a42e1fbdae29bbe878
jsm33t <jasmeetsinghkainth@gmail.com>
2026-04-02 21:31:01 +0530

    redis key updates

--- api/JassSpace.Api/Configuration/RedisCacheKeys.cs
+++ api/JassSpace.Api/Configuration/RedisCacheKeys.cs
@@ -2,10 +2,12 @@ namespace JassSpace.Api.Configuration;
 
 public static class RedisCacheKeys
 {
-    public const string BlogCategory = "blogcategory";
-    public const string BlogList = "bloglist";
-    public const string BlogSeo = "blogseo";
-    public const string GallerySeo = "galleryseo";
-    public const string MusicSeo = "musicseo";
-    public const string UiProperties = "uiproperties";
+    public const string Prefix = "jassspace_";
+
+    public const string BlogCategory = Prefix + "blogcategory";
+    public const string BlogList = Prefix + "bloglist";
+    public const string BlogSeo = Prefix + "blogseo";
+    public const string GallerySeo = Prefix + "galleryseo";
+    public const string MusicSeo = Prefix + "musicseo";
+    public const string UiProperties = Prefix + "uiproperties";
 }
